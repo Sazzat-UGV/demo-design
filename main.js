de606311fed125207b12ee3cdbe08726cb308c73
Asikul Islam Sazzat <asikulislamsazzat@gmail.com>
2025-03-28 17:46:58 +0600

    fixing some bugs

--- main.js
+++ main.js
@@ -1,16 +1,38 @@
-const menuBtn = document.getElementById('menu-btn');
-const navLinks = document.getElementById('nav_links');
-const menuBtnIcon = menuBtn.querySelector('i');
+const menuBtn = document.getElementById("menu-btn");
+const navLinks = document.getElementById("nav-links");
+const menuBtnIcon = menuBtn.querySelector("i");
 
-menuBtn.addEventListener('click', (e) => {
-navLinks.classList.toggle("open");
-
-const isOpen = navLinks.classList.contains('open');
-menuBtnIcon.setAttribute("class", isOpen ? "ri-close-line" : "ri-menu-line");
+menuBtn.addEventListener("click", (e) => {
+  navLinks.classList.toggle("open");
+  const isOpen = navLinks.classList.contains("open");
+  menuBtnIcon.setAttribute("class", isOpen ? "ri-close-line" : "ri-menu-line");
 });
 
+navLinks.addEventListener("click", (e) => {
+  navLinks.classList.remove("open");
+  menuBtnIcon.setAttribute("class", "ri-menu-line");
+});
 
-navLinks.addEventListener("click",(e)=>{
-navLinks.classList.remove('open');
-menuBtnIcon.setAttribute('class','ri-menu-line');
+const ScrollRevealOption = {
+  distance: "50px",
+  origin: "bottom",
+  duration: 1000,
+};
+ScrollReveal().reveal(".header_image img", {
+  ...ScrollRevealOption,
+});
+
+ScrollReveal().reveal(".header_content h4, .header_content, .section_header", {
+  ...ScrollRevealOption,
+  delay: 500,
+});
+
+ScrollReveal().reveal(".header_container p", {
+  ...ScrollRevealOption,
+  delay: 1000,
+});
+
+ScrollReveal().reveal(".header_btn", {
+  ...ScrollRevealOption,
+  delay: 1500,
 });
